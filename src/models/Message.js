ebd97fb08ddb789b1cb90fb6a7da227b4abf8768
Robin <sultanazakia711@gmail.com>
2024-10-28 18:34:29 +0600

    Created Message Schema

--- src/models/Message.js
+++ src/models/Message.js
@@ -1,23 +1,4 @@
-// const mongoose = require("mongoose");
 
-// const MessageSchema = new mongoose.Schema(
-//     {
-//         conversationId: {
-//             type: String,
-//         },
-//         sender: {
-//             type: String,
-//         },
-//         text: {
-//             type: String,
-//         },
-        
-//     },
-//     { timestamps: true }
-// );
-
-// const Message = mongoose.model("message", MessageSchema);
-// module.exports = Message;
 
 const mongoose = require("mongoose");
 

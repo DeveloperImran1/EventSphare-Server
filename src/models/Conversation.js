3c4767635ac591efc7a8e9e6958bbddd71d61f50
Robin <sultanazakia711@gmail.com>
2024-10-28 18:33:59 +0600

    Created conversationSchema

--- src/models/Conversation.js
+++ src/models/Conversation.js
@@ -1,16 +1,4 @@
-// const mongoose = require("mongoose");
 
-// const ConversationSchema = new mongoose.Schema(
-//   {
-//     members: {
-//         type: [String],
-//     },
-// },
-// { timestamps: true }
-// );
-
-// const Conversation = mongoose.model("Conversation", ConversationSchema);
-// module.exports = Conversation;
 
 const mongoose = require("mongoose");
 
